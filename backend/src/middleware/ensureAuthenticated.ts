205432e076f10045d2c2e7d79dce172ba737f073
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-12-07 23:24:58 -0300

    Configurando as tipagens customizadas

---
+++ backend/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+interface ITokenPayload {
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret) as ITokenPayload;
+
+    request.client = {
+      id: decoded.sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token');
+  }
+}
+
+export default ensureAuthenticated;
